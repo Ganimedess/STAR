cd5d74f69a1266c7680dab1e614c71da1270a3e3
Said Sel-lam <ssp.mays@gmail.com>
2023-07-27 10:57:08 +0200

    roble link

--- app/src/components/RoblePage.jsx
+++ app/src/components/RoblePage.jsx
@@ -20,7 +20,7 @@ const RoblePage = () => {
             <h2 className="text-xl italic mb-10">Holistic therapy centre</h2>
             <span className='text-slate-400'>Description:</span>
             <p className="text-xl">'El Roble' is a non-profit association focused on complementary therapies and natural medicine. In this web project, they aimed to promote the association, its philosophy, and the activities they carry out.</p>
-            <a rel='noreferrer' target='_blank' href="https://eclectic-mermaid-5f918e.netlify.app/"><button className="border-2 border-white px-2 py-1 rounded-lg hover:bg-white hover:text-black mt-5">Visit -&gt;</button></a>
+            <a rel='noreferrer' target='_blank' href="https://elroble-terapias.netlify.app/"><button className="border-2 border-white px-2 py-1 rounded-lg hover:bg-white hover:text-black mt-5">Visit -&gt;</button></a>
             <Link to="/work/Freakit!"><span className='ml-7 text-slate-400 underline hover:text-white'>Go to next proyect -&gt;</span></Link>
           </div>
           <div className="col-span-10 md:col-span-4 col-start-2 md:col-start-8 min-[1300px]:col-start-7 mb-12">
