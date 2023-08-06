c2d790bbbf0ffe5f8baf182b65b2959247531217
Said Sel-lam <ssp.mays@gmail.com>
2023-08-06 16:06:28 +0200

    retoques color

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
 
     return (
-    <section className="flex items-center min-h-screen z-40">
+    <section className="flex items-center min-h-[95vh] z-40">
         <div id="hero" className="grid grid-cols-12 gap-4 w-full py-48 max-md:py-0">
             <div className="col-start-2 lg:col-start-3 col-span-10 lg:col-span-8 grid grid-cols-8 gap-4 gap-y-8">
                 <div className="col-span-8 text-white leading-normal text-4xl md:leading-normal md:text-5xl lg:leading-normal lg:text-6xl">
--- app/src/components/OverlayLong.jsx
+++ app/src/components/OverlayLong.jsx
@@ -4,68 +4,65 @@ import Star from './Star'
 
 const OverlayLong = ({appHeight}) => {
 
-  const colors2 = { nRed: "#D44D5C", nBlue: "#51aeff", nYellow: "#DBFEB8", nPurple: "#8b58be"}
-  const colors3 = { nRed: "#A06B9A", nBlue: "#51D6FF", nYellow: "#37FF8B", nPurple: "#522B29"}
-  const colors1 = { nRed: "#FF99C8", nBlue: "#DBFEB8", nYellow: "#F37748", nPurple: "#32292F"}
+  const colors1 = { c1: "#8A817C", c2: "#bcb8b1", c3: "#f4f3ee", c4: "#e0afa0"}
+  const colors2 = { c1: "#bbcea8", c2: "#e3d87e", c3: "#f0ec57", c4: "#748067"}
+  const colors3 = { c1: "#fb9f89", c2: "#c4af9a", c3: "#81ae9d", c4: "#21a179"}
 
   const currentPalette = colors2
 
   return (
       <div id="overlay" className="fixed fixed w-full min-h-screen">
         <div className='parallax p-far static flex flex-wrap'>
-          <Star size={48} top={75} left={35} color={currentPalette.nRed} hid={true} />
-          <Ellipse size={20} top={7} left={60} color={currentPalette.nYellow} hid={true} />
-          <Ellipse size={16} top={40} left={14} color={currentPalette.nYellow} hid={true} />
-          <Ellipse size={28} top={70} left={52} color={currentPalette.nPurple} />
-          <Ellipse size={30} top={30} left={4} color={currentPalette.nPurple} hid={true} />
-          <Star size={22} top={25} left={80} color={currentPalette.nPurple} />
-          <Star size={18} top={85} left={10} color={currentPalette.nPurple} />
-          <Star size={28} top={65} left={85} color={currentPalette.nRed} hid={true} />
-          <Star size={24} top={20} left={30} color={currentPalette.nBlue} hid={true} />
-          <Star size={26} top={90} left={76} color={currentPalette.nBlue} />
+          <Star size={48} top={75} left={35} color={currentPalette.c1} hid={true} />
+          <Ellipse size={16} top={40} left={14} color={currentPalette.c3} hid={true} />
+          <Ellipse size={28} top={70} left={52} color={currentPalette.c4} />
+          <Ellipse size={30} top={30} left={4} color={currentPalette.c4} hid={true} />
+          <Star size={22} top={25} left={80} color={currentPalette.c4} />
+          <Star size={18} top={85} left={10} color={currentPalette.c4} />
+          <Star size={28} top={65} left={85} color={currentPalette.c1} hid={true} />
+          <Star size={24} top={10} left={30} color={currentPalette.c2} hid={true} />
+          <Star size={26} top={90} left={76} color={currentPalette.c2} />
           {/* long part */}
-          <Star size={24} top={120} left={25} color={currentPalette.nRed} hid={true} />
-          <Ellipse size={30} top={130} left={4} color={currentPalette.nYellow} />
-          <Star size={22} top={125} left={80} color={currentPalette.nYellow} />
-          <Ellipse size={16} top={140} left={14} color={currentPalette.nPurple} />
-          <Star size={48} top={155} left={35} color={currentPalette.nPurple} />
-          <Star size={48} top={175} left={35} color={currentPalette.nRed} hid={true} />
-          <Ellipse size={20} top={117} left={60} color={currentPalette.nYellow} hid={true} />
-          <Ellipse size={16} top={140} left={14} color={currentPalette.nYellow} hid={true} />
-          <Ellipse size={28} top={170} left={52} color={currentPalette.nPurple} />
-          <Ellipse size={30} top={130} left={4} color={currentPalette.nPurple} hid={true} />
-          <Star size={22} top={225} left={80} color={currentPalette.nPurple} />
-          <Star size={18} top={285} left={10} color={currentPalette.nPurple} />
-          <Star size={28} top={265} left={85} color={currentPalette.nRed} hid={true} />
-          <Star size={24} top={220} left={30} color={currentPalette.nBlue} hid={true} />
-          <Star size={26} top={190} left={76} color={currentPalette.nBlue} />
+          <Star size={24} top={130} left={25} color={currentPalette.c1} hid={true} />
+          <Ellipse size={30} top={130} left={4} color={currentPalette.c3} />
+          <Star size={22} top={125} left={80} color={currentPalette.c3} />
+          <Ellipse size={16} top={140} left={14} color={currentPalette.c4} />
+          <Star size={48} top={255} left={10} color={currentPalette.c4} />
+          <Star size={48} top={175} left={15} color={currentPalette.c1} hid={true} />
+          <Ellipse size={20} top={17} left={60} color={currentPalette.c3} hid={true} />
+          <Ellipse size={16} top={140} left={14} color={currentPalette.c3} hid={true} />
+          <Ellipse size={28} top={170} left={52} color={currentPalette.c4} />
+          <Star size={22} top={225} left={98} color={currentPalette.c4} />
+          <Star size={18} top={285} left={10} color={currentPalette.c4} />
+          <Star size={28} top={265} left={85} color={currentPalette.c1} hid={true} />
+          <Star size={24} top={250} left={89} color={currentPalette.c2} hid={true} />
+          <Star size={26} top={190} left={76} color={currentPalette.c2} />
 
 
         </div>
         <div className='parallax p-mid static flex flex-wrap'>
         </div>
         <div className='parallax p-close static flex flex-wrap'>
-          <Ellipse size={350} top={82} left={-4} color={currentPalette.nPurple} hid={true} />
-          <Ellipse size={180} top={22} left={94} color={currentPalette.nBlue} />
-          <Star size={100} top={-2} left={37} color={currentPalette.nPurple} />
-          <Star size={100} top={90} left={66} color={currentPalette.nBlue} hid={true} />
+          <Ellipse size={350} top={82} left={-4} color={currentPalette.c4} hid={true} />
+          <Ellipse size={180} top={22} left={94} color={currentPalette.c2} />
+          <Star size={100} top={-2} left={37} color={currentPalette.c4} />
+          <Star size={100} top={90} left={66} color={currentPalette.c2} hid={true} />
           {/* long part */}
-          <Ellipse size={110} top={130} left={46} color={currentPalette.nRed} hid={true} />
-          <Ellipse size={350} top={182} left={-4} color={currentPalette.nPurple} hid={true} />
-          <Ellipse size={180} top={222} left={94} color={currentPalette.nBlue} />
-          <Star size={90} top={180} left={79} color={currentPalette.nYellow} />
-          <Star size={100} top={212} left={37} color={currentPalette.nPurple} />
-          <Star size={100} top={290} left={66} color={currentPalette.nBlue} hid={true} />
+          <Ellipse size={110} top={130} left={46} color={currentPalette.c1} hid={true} />
+          <Ellipse size={350} top={182} left={-4} color={currentPalette.c4} hid={true} />
+          <Ellipse size={180} top={222} left={94} color={currentPalette.c2} />
+          <Star size={90} top={180} left={79} color={currentPalette.c3} />
+          <Star size={100} top={290} left={66} color={currentPalette.c2} hid={true} />
 
-          <Ellipse size={120} top={517} left={60} color={currentPalette.nYellow} hid={true} />
-          <Ellipse size={116} top={240} left={14} color={currentPalette.nYellow} hid={true} />
-          <Ellipse size={128} top={270} left={52} color={currentPalette.nPurple} />
-          <Ellipse size={130} top={330} left={4} color={currentPalette.nPurple} hid={true} />
-          <Star size={122} top={325} left={80} color={currentPalette.nPurple} />
-          <Star size={118} top={385} left={10} color={currentPalette.nPurple} />
-          <Star size={128} top={365} left={85} color={currentPalette.nRed} hid={true} />
-          <Star size={124} top={420} left={30} color={currentPalette.nBlue} hid={true} />
-          <Star size={126} top={390} left={76} color={currentPalette.nBlue} />
+          <Ellipse size={120} top={517} left={5} color={currentPalette.c3} hid={true} />
+          <Ellipse size={116} top={240} left={14} color={currentPalette.c3} hid={true} />
+          <Ellipse size={128} top={270} left={52} color={currentPalette.c4} />
+          <Ellipse size={130} top={330} left={4} color={currentPalette.c4} hid={true} />
+          <Star size={122} top={325} left={80} color={currentPalette.c4} />
+          <Star size={118} top={385} left={10} color={currentPalette.c4} />
+          <Star size={128} top={465} left={85} color={currentPalette.c1} hid={true} />
+          <Star size={124} top={440} left={-3} color={currentPalette.c2} hid={true} />
+          <Star size={126} top={390} left={76} color={currentPalette.c2} />
         </div>
       </div>
   )
