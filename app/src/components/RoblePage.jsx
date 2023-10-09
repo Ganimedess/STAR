01f8144875fc8c4c1c2b3eb471a6e41161a21c4b
Said Sel-lam <ssp.mays@gmail.com>
2023-10-09 18:53:54 +0200

    .

--- app/src/components/RoblePage.jsx
+++ app/src/components/RoblePage.jsx
@@ -13,16 +13,16 @@ const RoblePage = () => {
     <>
       <section id='proyect' className="aparece min-h-[50vh] pt-24 flex items-center">
         <div className="grid grid-cols-12 gap-4 w-full">
-          <div className="col-span-10 col-start-2">
+          <div className="col-span-8 col-start-3">
               <h1 className="text-6xl font-bold">El Roble</h1>
           </div>
 
-          <div className="col-span-10 md:col-span-5 col-start-2 md:col-start-2 mb-5 md:mb-12">
+          <div className="col-span-10 md:col-span-4 col-start-2 md:col-start-3 mb-5 md:mb-12">
             <h2 className="text-xl italic mb-10">Holistic therapy centre</h2>
             <span className='text-slate-900'>Description:</span>
             <p className="text-xl">'El Roble' is a non-profit association focused on complementary therapies and natural medicine. In this web project, they aimed to promote the association, its philosophy, and the activities they carry out.</p>
             <a rel='noreferrer' target='_blank' href="https://elroble.uno/"><button className="biggie text-xl px-4 py-1 rounded-lg bg-white hover:bg-verde mt-9">Visit →</button></a>
-            <Link to="/work/Freakit!" onClick={gotop}><span className='biggie text-xl ml-7 text-slate-900 hover:bg-white px-2'>Go to next proyect →</span></Link>
+            <Link to="/work/Freakit!" onClick={gotop}><span className='biggie text-xl ml-7 text-slate-900 hover:bg-verde px-2'>Go to next proyect →</span></Link>
           </div>
           <div className="col-span-10 md:col-span-4 col-start-2 md:col-start-8 mb-12">
           <span className='text-slate-900'>Type:</span>
