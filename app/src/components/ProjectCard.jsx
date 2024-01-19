31dd82f02766e6bc7768cce7d8fb2c3ff4c275f1
Said Sel-lam <ssp.mays@gmail.com>
2024-01-19 03:14:59 +0100

    custom cursor for projects

--- app/src/components/ProjectCard.jsx
+++ app/src/components/ProjectCard.jsx
@@ -1,13 +1,22 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import {useState} from 'react'
 
 const ProjectCard = ({thumbnail, link , title , description}) => {
     function scrollTop() {
         document.querySelector(".App").scrollTo(0,0);
     }
+    const [eyeOpen, setEye] = useState(true);
+
+    const closeEye = () => {
+        setEye(false)
+    }
+    const openEye = () => {
+        setEye(true)
+    }
 
     return(
-    <Link onClick={scrollTop} to={link} className='project round10 text-slate-900 col-span-4 bg-[#ffffffe0] hover:bg-white overflow-hidden'>
+    <Link onClick={scrollTop} to={link} className={`${eyeOpen ? 'eyeOpen' : 'eyeClosed'} round10 project text-slate-900 col-span-4 bg-[#ffffffe0] hover:bg-white overflow-hidden`} onMouseDown={closeEye} onMouseUp={openEye}>
         <div className="items-stretch flex flex-col h-full">
             <div className="col-span-full md:col-span-6 flex flex-col justify-center md:text-left p-2 md:p-5 md:px-7">
                 <h6 className="text-2xl md:text-4xl font-bold block fontCool">{title}</h6>
--- app/src/components/Work.jsx
+++ app/src/components/Work.jsx
@@ -4,19 +4,29 @@ import freakitThumbnail from '../assets/thumb-freakit.png'
 import elRobleThumbnail from '../assets/thumb-roble.png'
 import centromcThumbnail from '../assets/thumb-centromc.png'
 import BeThumbnail from '../assets/thumb-be.png'
+import {useState} from 'react'
 
 const Work = () => {
 
-  return (
+    const [eyeOpen, setEye] = useState(true);
+
+    const closeEye = () => {
+        setEye(false)
+    }
+    const openEye = () => {
+        setEye(true)
+    }
+
+    return (
     <section id="work" className="flex items-start bg-slate-900">
         <div className=" grid grid-cols-12 gap-4 w-full py-24 max-md:py-24">
 
-            <div className="aparece col-start-2 lg:col-start-3 col-span-10 lg:col-span-9  grid grid-cols-8 gap-2 md:gap-12 p-3 lg:p-0">
+            <div className="col-start-2 lg:col-start-3 col-span-10 lg:col-span-9  grid grid-cols-8 gap-2 md:gap-12 p-3 lg:p-0">
                 <p className="text-white  col-span-full text-2xl mb-6 md:mb-0">Selected projects:</p>
                 <ProjectCard thumbnail={centromcThumbnail} link='/work/centromc' title='Mímate y Cuídate' description='Luxury beauty center'/>
                 <ProjectCard thumbnail={freakitThumbnail} link='/work/freakit!' title='Freakit!' description='Landing for a dating app'/>
                 <ProjectCard thumbnail={elRobleThumbnail} link='/work/elRoble' title='El Roble' description='Alternative therapy center'/>
-                <a href='https://www.behance.net/said-sellam' rel='noreferrer' target='_blank' className='round10 project text-slate-900 col-span-4 bg-[#ffffffe0] hover:bg-white overflow-hidden'>
+                <a href='https://www.behance.net/said-sellam' rel='noreferrer' target='_blank' className={`${eyeOpen ? 'eyeOpen' : 'eyeClosed'} round10 project text-slate-900 col-span-4 bg-[#ffffffe0] hover:bg-white overflow-hidden`} onMouseDown={closeEye} onMouseUp={openEye}>
                     <div className="ititems-stretch flex flex-col h-full">
                         <div className="col-span-full md:col-span-6 flex flex-col justify-center text-center md:text-left p-2 md:p-5 md:px-7">
                             <h6 className="text-2xl md:text-4xl font-bold block fontCool">More projects...</h6>
@@ -31,7 +41,7 @@ const Work = () => {
             </div>
         </div>
     </section>
-  )
+    )
 }
 
 export default Work
