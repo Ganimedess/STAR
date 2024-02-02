412c34cbc58ce42710dc3ed6110a92691da9c427
Said Sel-lam <ssp.mays@gmail.com>
2024-02-02 02:15:25 +0100

    .

--- tareas/src/App.jsx
+++ tareas/src/App.jsx
@@ -4,7 +4,10 @@ import Lista from './components/Lista.jsx'
 
 function App() {
     /*
-    AQUÍ UN SWITCH PARA CAMBIAR DE TAREAS A RUTINAS
+    -añadir el switch para cambiar a rutinas
+    -añadir lista de rutinas
+    -añadir posibilidad de editar y borrar tareas
+    -añadir lógica común para crear un solo objeto con la info de tareas
     */
   return (
     <>
